770bc3f3c84ac29cd3b409bd6f0a80f534b97726
Your Name <edward.lenzner@gmail.com>
2024-02-05 13:19:49 +1100

    k

--- commands/games/player.js
+++ commands/games/player.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const {Game, Player} = require('./data/util.js');
 const fs = require('node:fs');
 const path = require('node:path');
@@ -51,11 +51,55 @@ module.exports = {
         const data = require('./data/data.json')
         const game = Game.toGame(JSON.parse(data[interaction.options.getUser("host").id]))
         const player = game.players[game.players.map(obj => obj.discord.username).indexOf(interaction.options.getString('player_name'))]
+        const embed = new EmbedBuilder()
+            .setTitle(player.discord.username)
+            .setDescription(`Game: ${game.name} (Host: ${game.owner.username})\n\n__**Status:**__\n**Health:** ${player.status.health}/100\n**Hunger:** ${player.status.hunger}\n**Thirst:** ${player.status.thirst}`)
+            .setThumbnail(player.image)
+
+        const inventory = new ButtonBuilder()
+            .setCustomId('inventory')
+            .setLabel('Inventory')
+            .setStyle(ButtonStyle.Primary);
+        const actions = new ButtonBuilder()
+            .setCustomId('actions')
+            .setLabel('Actions')
+            .setStyle(ButtonStyle.Primary)
+        const status = new ButtonBuilder()
+            .setCustomId('status')
+            .setLabel('Status')
+            .setStyle(ButtonStyle.Primary)
+        const srow = new ActionRowBuilder().addComponents(inventory, actions)
+        const irow = new ActionRowBuilder().addComponents(status, actions)
+        const arow = new ActionRowBuilder().addComponents(status, inventory)
+
         if(interaction.options.getSubcommand() === "view") {
-            const embed = new EmbedBuilder()
-                .setTitle(player.discord.username)
-                .setDescription(`Game: ${game.name} (Host: ${game.owner.username})\n\nStatus:\n**Health:** ${player.status.health}/100\n**Hunger:** ${player.status.hunger}\n**Thirst:** ${player.status.thirst}`)
-                .setThumbnail(player.image)
+            var response = await interaction.reply({
+                embeds: [embed],
+                components: [srow],
+            });
+            
+            const collectorFilter = i => i.user.id === interaction.user.id;
+            let active = true
+
+            try {
+                while(active) {
+                    const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+                    if (confirmation.customId === 'inventory') {
+                        response = await interaction.reply({
+                            embeds: [embed],
+                            components: [irow],
+                        });
+                    } else if (confirmation.customId === 'actions') {
+                        response = await confirmation.update({
+                            embeds: [embed],
+                            components: [arow],
+                        });
+                    }
+                }
+            } catch (e) {
+                active = false
+                await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+            }
         } else {
             await interaction.reply(`mew ${interaction.options.getUser("host").username}, ${interaction.options.getString('player_name')}`)
         }
